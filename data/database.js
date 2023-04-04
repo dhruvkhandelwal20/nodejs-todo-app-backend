dd487654e34ea14894de10f26549796813b24b2a
Dhruv Khandelwal <dhruvkhandelwal39@gmail.com>
2023-04-04 17:12:42 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,8 +5,8 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "todoProjectDB",
     })
-    .then(() => {
-      console.log("Database Connected!");
+    .then((c) => {
+      console.log(`Database Connected with ${c.connection.host}`);
     })
     .catch((error) => {
       console.log(error);
